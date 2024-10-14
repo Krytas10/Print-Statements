24e72e5fa647331be5f468e867df8bcff06ed12b
Krytas10 <krytasplays@gmail.com>
2024-10-14 10:01:31 -0500

    Add files via upload

---
+++ CL1 Print Statements.py
@@ -0,0 +1,90 @@
+#1 Basic String OUtput
+print("Hello World!")
+
+
+#2 Simple Math
+a = 10                                                          
+b = 15
+c = a+b
+print(c)
+
+
+#3 Multiple Arguments
+word1 = "Python"
+word2 = "is"
+word3 = "Fun!"
+print(word1 + " " + word2 + " " + word3)
+
+
+#4 Escape Characters 
+print("Zameer said, \"Hello!\"")
+
+
+#5 NewLine Character
+print("Player 1\nPlayer 2")
+
+
+#6 Tab Characters
+print("Name   \t Year \t    Program")
+print("Zameer \t  1   \t Cyber Security")
+
+
+#7 Multuple data types 
+text = "the result is "
+int = 42
+print(text, int)
+
+
+#8 Single Number
+print(1,2,3,4,5)
+
+
+#9 Decimal Number
+print(3.14159)
+
+
+#10 Special Character
+print("$")
+
+
+#11 Sep Parameter
+print("apple", "banana", "cherry", sep=", ")
+
+
+#12 End Parameter
+print("Hello,", end=" ")
+print("World!")
+
+
+#13 Repeated String
+print("Python! " * 3)
+
+
+#14 Unicode Character
+print("\u2764") # Prints a heart symbol
+
+
+#15 Empty line 
+print()
+
+
+#16 Concatenating Strin
+print("Hello " + "World!")
+
+
+#17 Boolean Values
+print(True)
+print(False)
+
+
+#18 Exponentioal Notation
+print(1.2e3) 
+
+
+#19 Multiline text
+print("""Bath Spa
+Rak Campus.""")
+
+
+#20 Escaped Backslash
+print("This is a backslash: \\")
